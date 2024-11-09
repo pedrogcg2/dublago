8ad2f00881ef7994a59d6c26a76ea1348bb9354b
Arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-11-09 13:36:50 -0300

    [TDC] Using Option Pattern in TTS module to a code more flexible

--- tts/coquitts.go
+++ tts/coquitts.go
@@ -5,36 +5,74 @@ import (
 	"fmt"
 	"log/slog"
 	"os/exec"
+	"path/filepath"
 	"time"
-	"tradutor-dos-crias/shared"
 
 	"github.com/google/uuid"
 )
 
-type CoquiTTS struct{}
+type CoquiTTS struct {
+	modelName      string
+	speakerWavPath string
+	languageIdx    string
+}
 
-func (c CoquiTTS) Speech(message string) (string, error) {
+type Option func(*CoquiTTS)
+
+func NewCoquiTTS(opts ...Option) *CoquiTTS {
+	cq := new(CoquiTTS)
+
+	cq.modelName = "tts_models/multilingual/multi-dataset/xtts_v2"
+	cq.speakerWavPath, _ = filepath.Abs("pipe/primegean-speaker.wav")
+	cq.languageIdx = "pt"
+
+	for _, o := range opts {
+		o(cq)
+	}
+
+	return cq
+}
+
+func WithModelName(modelName string) Option {
+	return func(ct *CoquiTTS) {
+		ct.modelName = modelName
+	}
+}
+
+func WithSpeakerWavPath(speakerWavRelativePath string) Option {
+	return func(ct *CoquiTTS) {
+		ct.speakerWavPath, _ = filepath.Abs(speakerWavRelativePath)
+	}
+}
+
+func WithLanguageIdx(languageIdx string) Option {
+	return func(ct *CoquiTTS) {
+		ct.languageIdx = languageIdx
+	}
+}
+
+func (c *CoquiTTS) Speech(message string) (string, error) {
 	slog.Info("[TTS] Generating speech now")
-	modelName := "tts_models/multilingual/multi-dataset/xtts_v2"
-
-	speakerWavPath := shared.PWD + "pipe/primegean-speaker.wav"
 
 	t := time.Now()
 	outFile := fmt.Sprintf("%d-%d-%d-%s-tts.wav", t.Day(), t.Month(), t.Year(), uuid.New())
-	outPath := shared.PWD + "pipe/" + outFile
+	outPath, err := filepath.Abs("pipe/" + outFile)
+	if err != nil {
+		slog.Error(`[TTS] Error in getting absolute outPath path: ` + err.Error())
+		return "", err
+	}
 
-	languageIdx := "pt"
-
-	cmd := exec.Command("tts", "--model_name", modelName,
+	cmd := exec.Command("tts",
+		"--model_name", c.modelName,
 		"--text", message,
-		"--speaker_wav", speakerWavPath,
+		"--speaker_wav", c.speakerWavPath,
 		"--out_path", outPath,
-		"--language_idx", languageIdx)
+		"--language_idx", c.languageIdx)
 
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		slog.Error(`[TTS] Error in running 'cmd.Run': ` + err.Error() +
 			"\n\t\t\tCLI Error Output: " + stderr.String())
